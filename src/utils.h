5b80435beaec2b495eb0d9220106ac96c6cb8739
Felipe Behling <89941251+felipebehling@users.noreply.github.com>
2025-09-09 14:38:12 -0300

    Create utils.h

---
+++ src/utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "hardstress.h"
+
+double now_sec(void);
+uint64_t splitmix64(uint64_t *x);
+void shuffle32(uint32_t *a, size_t n, uint64_t *seed);
+
+int thread_create(thread_handle_t *t, thread_func_t func, void *arg);
+int thread_join(thread_handle_t t);
+int thread_detach(thread_handle_t t);
+
+#endif // UTILS_H
