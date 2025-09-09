07d9af2d51ce7eb4554c5611994452bc2958179f
Felipe Behling <89941251+felipebehling@users.noreply.github.com>
2025-09-09 19:48:09 -0300

    Update ui.h

--- src/ui.h
+++ src/ui.h
@@ -5,5 +5,7 @@
 
 GtkWidget* create_main_window(AppContext *app);
 void gui_log(AppContext *app, const char *fmt, ...);
+// ADICIONADO: Declaração da função para ser usada externamente
+gboolean gui_update_stopped(gpointer ud);
 
 #endif // UI_H
